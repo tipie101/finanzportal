1763851b443a77ed4f98e785b7e815116451a9d1
tipie101 <timka.timkah@gmail.com>
2020-08-15 17:18:38 +0200

    calculating annuity based on anfangstilgung

--- backend/redemptionplan/src/main/java/com/finanzportal/redemptionplan/RedemptionPlanController.java
+++ backend/redemptionplan/src/main/java/com/finanzportal/redemptionplan/RedemptionPlanController.java
@@ -16,17 +16,18 @@ public class RedemptionPlanController {
     public String calculate(
         @PathParam("betrag") String betrag,
         @PathParam("zinssatz") String zinssatz,
-        // TODO: What is anfangstilgung? betrag - anfangstilgung = darlehen (???)
+        // TODO: What is 'anfangstilgung'? maybe 'tilgungssatz'?
         @PathParam("anfangstilgung") String anfangstilgung) {
 
         // TODO: Check Params
 
         double betragNumeric = convertGermanNumber(betrag);
+        double anfangstilgungNumeric = convertGermanNumber(anfangstilgung);
         double zinsNumeric = convertGermanNumber(zinssatz);
 
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.GERMAN);
-        double annuity = calculateAnnuity(zinsNumeric, betragNumeric, 10);
-        return numberFormat.format(annuity);
+        double annuity = calculateAnnuity(zinsNumeric, betragNumeric, anfangstilgungNumeric);
+        return numberFormat.format(annuity/12.0);
     }
 
     public double convertGermanNumber(String number) {
@@ -34,9 +35,8 @@ public class RedemptionPlanController {
         return Double.parseDouble(number.replace(".", "").replace(',', '.'));
     }
 
-    public double calculateAnnuity(double zinssatz, double fund, int duration) {
-        double q = 1.0 + zinssatz/100.0;
-        return ( fund * Math.pow(q, duration) * (q - 1.0) ) / (Math.pow(q, duration) - 1);
+    public double calculateAnnuity(double zinssatz, double fund, double tilgung) {
+        return fund * (tilgung/100.0 + zinssatz/100.0);
     }
 
 }

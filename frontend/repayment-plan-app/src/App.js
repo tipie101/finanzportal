c24304a94736634460ee5cd603ebd96ae7d60b3e
tipie101 <timka.timkah@gmail.com>
2020-08-19 13:26:33 +0200

    layout: header

--- frontend/repayment-plan-app/src/App.js
+++ frontend/repayment-plan-app/src/App.js
@@ -6,18 +6,22 @@ import RepaymentForm from './RepaymentForm';
 function App() {
   return (
     <div className="App">
-      <div class="container">
-        <div class="row">
-          {/* HEADER */}
+
+        <div class="header">
           <h2>
-            .
+          RECHNER FÜR DEN DARLEHENS-TILGUNGSPLAN
           </h2>
-        </div>
+        </div>  
+
         <div class="row">
-          <RepaymentForm />
+          <div class="col-sm-2"></div>
+          <div class="col-sm-8">
+            <RepaymentForm />
+          </div>
+          <div class="col-sm-2"></div>
         </div>
-      </div>
     </div>
+    
   );
 }
 
